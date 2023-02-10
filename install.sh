7b36848762632844489bcabdc30b701dbc56e04e
MonPham <demondhunter93@gmail.com>
2023-02-10 22:35:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@ tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz
 # cp -r ./meson_cdn-linux-amd64/* . && rm -rf meson_cdn-linux-amd64
 cd meson_cdn-*
 # sudo chmod +x ./service && sudo chmod +x ./meson_cdn
-sudo mkdir /etc/init/ && sudo touch /etc/init/meson_cdn.conf
+ mkdir /etc/init/ && sudo touch /etc/init/meson_cdn.conf
 
-sudo ./service remove meson_cdn 
-sudo ./service install meson_cdn
-sudo ./service status meson_cdn 
+ ./service remove meson_cdn 
+ ./service install meson_cdn
+ ./service status meson_cdn 
 
-sudo ./meson_cdn config set --token=$TOKEN --https_port=443 --cache.size=30
+ ./meson_cdn config set --token=$TOKEN --https_port=443 --cache.size=30
 
-sudo ./service restart meson_cdn 
-sudo ./service status meson_cdn  
-sudo ./meson_cdn log 
+ ./service restart meson_cdn 
+ ./service status meson_cdn  
+ ./meson_cdn log 

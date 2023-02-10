48e11860c6b990b897c42035157350e48e359939
MonPham <demondhunter93@gmail.com>
2023-02-10 22:39:00 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 wget 'https://staticassets.meson.network/public/meson_cdn/v3.1.19/meson_cdn-linux-amd64.tar.gz' && \
 tar -zxf meson_cdn-linux-amd64.tar.gz && rm -f meson_cdn-linux-amd64.tar.gz 
 # cp -r ./meson_cdn-linux-amd64/* . && rm -rf meson_cdn-linux-amd64
-cd meson_cdn-*
+cd meson_cdn-* && ls 
 # sudo chmod +x ./service && sudo chmod +x ./meson_cdn
- mkdir /etc/init/ && sudo touch /etc/init/meson_cdn.conf
+ mkdir /etc/init/ &&  touch /etc/init/meson_cdn.conf
 
  ./service remove meson_cdn 
  ./service install meson_cdn

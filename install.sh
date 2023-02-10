780f48ce75a70993a81c52865433f6c9eb2e4a56
MonPham <demondhunter93@gmail.com>
2023-02-10 21:57:35 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 chmod +x ./service && chmod +x ./meson_cdn
+mkdir /etc/init/ && touch /etc/init/meson_cdn.conf
 ./meson_cdn config set --token=$TOKEN --https_port=443 --cache.size=30
 ./service install meson_cdn
